6aeeb735235728763545914630f9c37e007c39c5
buglass <38510232+buglass@users.noreply.github.com>
2025-10-28 20:21:24 +0000

    add patient record controller with test implementations

--- PatientSummaryRecord.Tests/Controllers/PatientRecordControllerTests.cs
+++ PatientSummaryRecord.Tests/Controllers/PatientRecordControllerTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientSummaryRecord.Controllers;
+using PatientSummaryRecord.Models;
+using Xunit;
 
 namespace PatientSummaryRecord.Tests.Controllers
 {
@@ -7,13 +10,36 @@ namespace PatientSummaryRecord.Tests.Controllers
 		[Fact]
 		public void ExistentPatientReturns200WithRecord()
 		{
+			const int patientId = 1;
+			const int expectedStatusCode = 200;
+			var expectedData = new PatientDto
+			{
+				Id = patientId,
+				NHSNumber = "12345678",
+				Name = "mock",
+				DateOfBirth = new System.DateTime(2020, 01, 31),
+				GPPractice = "North Bank"
+			};
 
+			var actual = new PatientRecordController().Get(patientId);
+
+			Assert.IsType<OkObjectResult>(actual);
+			var okResult = (OkObjectResult)actual;
+			Assert.Equal(expectedStatusCode, okResult.StatusCode);
+			Assert.Equal(expectedData, okResult.Value);
 		}
 
 		[Fact]
 		public void NonExistentPatientReturns404()
 		{
+			const int patientId = 1;
+			const int expectedStatusCode = 404;
 
+			var actual = new PatientRecordController().Get(patientId);
+
+			Assert.IsType<NotFoundResult>(actual);
+			var notFoundResult = (NotFoundResult)actual;
+			Assert.Equal(expectedStatusCode, notFoundResult.StatusCode);
 		}
 	}
 }
---
+++ PatientSummaryRecord/Controllers/PatientRecordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PatientSummaryRecord.Models;
+
+namespace PatientSummaryRecord.Controllers
+{
+	[Route("Patients")]
+	[ApiController]
+	public class PatientRecordController
+	{
+		[HttpGet("{id}")]
+		public IEnumerable<PatientDto> Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
