092f653a0789bf85d6e9e8508e1c430d8ffa2219
buglass <38510232+buglass@users.noreply.github.com>
2025-10-28 21:05:43 +0000

    correctly implement 404 and exception on GET

--- PatientSummaryRecord/Controllers/PatientRecordController.cs
+++ PatientSummaryRecord/Controllers/PatientRecordController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using PatientSummaryRecord.Models;
 using PatientSummaryRecord.Services;
 
 namespace PatientSummaryRecord.Controllers
@@ -19,11 +21,13 @@ namespace PatientSummaryRecord.Controllers
 		[HttpGet("{id}")]
 		public IActionResult Get(int id)
 		{
-			try {
-				return new OkObjectResult(_patientRecordRepository.SelectById(id).Single());
-			} catch (ArgumentNullException) {
+			IEnumerable<PatientDto> patients = _patientRecordRepository.SelectById(id);
+			if (!patients.Any())
 				return new NotFoundResult();
-			}
+			else if (patients.SingleOrDefault() == null)
+				throw new InvalidOperationException();
+			else
+				return new OkObjectResult(patients.Single());
 		}
 	}
 }
