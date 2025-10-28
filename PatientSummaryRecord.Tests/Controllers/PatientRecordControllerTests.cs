02cd93a3bb774d13ef1f428df31ea647c9bbeb3e
buglass <38510232+buglass@users.noreply.github.com>
2025-10-28 20:07:55 +0000

    add empty patient record controller unit tests

---
+++ PatientSummaryRecord.Tests/Controllers/PatientRecordControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace PatientSummaryRecord.Tests.Controllers
+{
+	public class PatientRecordControllerTests
+	{
+		[Fact]
+		public void ExistentPatientReturns200WithRecord()
+		{
+
+		}
+
+		[Fact]
+		public void NonExistentPatientReturns404()
+		{
+
+		}
+	}
+}
